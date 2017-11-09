48598f4c38740e6c9d72ec1c034fbb102b68116f
rcoliveira2016 <roramon7@gmail.com>
2017-11-08 22:47:21 -0200

    commit de parameytros

---
+++ src/Convert Playlist/ViewModel/Parametros/PesquisarPlaylistParametros.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Convert_Playlist.ViewModel.Parametros
+{
+    public class PesquisarPlaylistParametros
+    {
+        public string Url { get; set; }
+    }
+}
